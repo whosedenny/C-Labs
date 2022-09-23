d98b3699b0d81665efa989c1e34874188975bc50
whosedenny <90749476+whosedenny@users.noreply.github.com>
2022-09-23 16:30:36 +0300

    Add files via upload

---
+++ lb1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace lb1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                double R, R_1, R_2;
+                bool flag = false;
+                do
+                {
+                    Console.WriteLine("Введите значение первого резистора ");
+                    flag = double.TryParse(Console.ReadLine(), out R_1);
+                } while (R_1 <= 0 || !flag);
+                flag = false;
+                do
+                {
+                    Console.WriteLine("Введите значение второго резистора: ");
+                    flag = double.TryParse(Console.ReadLine(), out R_2);
+                } while (R_2 <= 0 || !flag);
+
+                R = (1 / R_1) + (1 / R_2);
+                Console.WriteLine("ОТВЕТ: " + R);
+                Console.ReadLine();
+            }
+        }
+    }
+}
