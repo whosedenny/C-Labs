f0c0ccf5206c6f2d5b02e483f0368ff7f3b64e7d
whosedenny <90749476+whosedenny@users.noreply.github.com>
2022-09-23 17:26:05 +0300

    Add files via upload

---
+++ lb2/Program1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace lb2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double i, n, k, a;
+            bool flag = false;
+            a = 0;
+            do
+            {
+                Console.WriteLine("Введите n: ");
+                flag = double.TryParse(Console.ReadLine(), out n);
+            } while (!flag);
+            k = n * n;
+            for (i = k; i <= n * k; i++)
+            {
+                a += (Math.Pow(k, 2) - 1) / (Math.Pow((-1), (k + 1)) * Math.Pow(k, 2) + 7);
+            }
+            Console.WriteLine("ОТВЕТ: " + a);
+        }
+    }
+}
