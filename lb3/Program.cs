6a47c45bb3f5eb82d58d32578f9068b7c30e418f
whosedenny <90749476+whosedenny@users.noreply.github.com>
2022-09-28 02:59:20 +0300

    Add files via upload

---
+++ lb3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace lb3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int f, s, t;
+            bool flag = false;
+            do
+            {
+                Console.WriteLine("Введіть перше число: ");
+                flag = int.TryParse(Console.ReadLine(), out f);
+            } while (f <= 0 || !flag);
+            flag = false;
+            do
+            {
+                Console.WriteLine("Введіть друге число: ");
+                flag = int.TryParse(Console.ReadLine(), out s);
+            } while (s <= 0 || !flag);
+            flag = false;
+            do
+            {
+                Console.WriteLine("Введіть третє число: ");
+                flag = int.TryParse(Console.ReadLine(), out t);
+            } while (t <= 0 || !flag);
+
+            if (f == s || s== t || t == f)
+            {
+                Console.WriteLine("Серед цих трьох цілих чисел є хоча б одна пара співпадаючих.");
+            }
+            else
+            {
+                Console.WriteLine("Серед цих трьох цілих чисел немає хоча б одної пара співпадаючих.");
+            }
+        }
+    }
+}
