2142c377c3209bb0914e5c082b771ee7ca6e42d8
whosedenny <90749476+whosedenny@users.noreply.github.com>
2022-10-05 00:32:06 +0300

    Add files via upload

---
+++ lb4/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lb4
+{
+    class Program
+    {
+ 
+        static void Main(string[] args)
+        {
+            int mostOtenNumber = 0;
+            int highestNumberOfRepetitions = 0;
+            int numberOfRepetitions = 0;
+            Console.Write("Введите длинну массива: ");
+
+            int[] arr = new int[int.Parse(Console.ReadLine())];
+
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write("Введите " + i + " элемент массива = ");
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+            foreach (int i in arr)
+            {
+                foreach (int j in arr)
+                {
+                    if (i == j)
+                    {
+                        numberOfRepetitions++;
+                    }
+                }
+
+                if (highestNumberOfRepetitions < numberOfRepetitions)
+                {
+                    highestNumberOfRepetitions = numberOfRepetitions;
+                    mostOtenNumber = i;
+                }
+                numberOfRepetitions = 0;
+            }
+            Console.WriteLine("Больше всего повторяется число {0}, количество его повторений равно {1}", mostOtenNumber, highestNumberOfRepetitions);
+            Console.ReadKey(true);
+        }
+    }
+}
