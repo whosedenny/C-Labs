7061d5da18e7ea6653cbb663bc1e1ee92bb93a56
whosedenny <90749476+whosedenny@users.noreply.github.com>
2022-10-19 04:29:18 +0300

    Add files via upload

---
+++ lb4/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lb4
+{
+    // Даний масив розміру N. Визначити кількість його проміжків монотонності.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 3, 5, 3, 2, 3, 5, 6, 6, 4 };
+
+            int count = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (i > 1)
+                {
+                    if ((arr[i] > arr[i - 1]) && (arr[i - 1] < arr[i - 2]))
+                    {
+                        count++;
+                    }
+                    else if ((arr[i] < arr[i - 1]) && (arr[i - 1] > arr[i - 2]))
+                    {
+                        count++;
+                    }
+                }
+            }
+
+            Console.WriteLine("Кiлькiсть  промiжкiв  монотонностi якi зростають - {0}", count);
+            Console.ReadLine();
+        }
+    }
+}
