37ab3b8b71c60fe9cddced16ddb23d1017f37458
whosedenny <90749476+whosedenny@users.noreply.github.com>
2022-12-25 17:23:32 +0200

    Add files via upload

---
+++ lb10/Extension.cs
@@ -0,0 +1,45 @@
+﻿using lb6;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lb6_extension
+{
+    public static class Extension
+    {
+        public static void Processing(this Reverse reverse, int value) 
+        {
+            value = Math.Abs(value);
+            double arithmetic = 0, geometric = 1;
+            int i = 0;
+
+            if (value < 10 && value >= 0)
+            {
+                Console.WriteLine("arithmetic mean = {0}\ngeometric mean = {0}", value);
+                return;
+            }
+            while (value != 0)
+            {
+                arithmetic += value % 10;
+                geometric *= value % 10;
+                i++;
+                value /= 10;
+            }
+            //do
+            //{
+            //    arithmetic += value % 10;
+            //    geometric *= value % 10;
+            //    i++;
+            //    value /= 10;
+            //} while (!(value < 10));
+
+            arithmetic /= i;
+            geometric = Math.Pow(geometric, 1.0/i);
+
+            Console.WriteLine("arithmetic mean = {0}\ngeometric mean = {1}",
+                arithmetic, geometric);
+        }
+    }
+}
---
+++ lb10/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using lb6_extension;
+
+namespace lb6;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Reverse reverse = new Reverse("abcd");
+
+        reverse.Processing(164694);
+    }
+}
+
+public class Reverse
+{
+    public string Sentence { get; set; }
+    public Reverse(string sentence)
+    {
+        Sentence = sentence;
+    }
+
+    public string ReverseSentence()
+    {
+        return Sentence.Reverse().ToString();
+    }
+}
